1acd3851187bbe0e9da8d52ee579e0b4888c9964
Wuestenfuchs88 <sven.stamm@students.fhnw.ch>
2015-05-26 00:19:57 +0200

    - Update Scatterplot: - 1. Funktionsweise steht. - Koordinatensystem muss noch verbessert werden, damit es für alle Daten stimmt. - Rotation noch auskommentiert.

--- src/main/java/analyzer/gui/ScatterPlot.java
+++ src/main/java/analyzer/gui/ScatterPlot.java
@@ -3,14 +3,15 @@ package analyzer.gui;
 import analyzer.datastore.Data;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
+import java.util.ArrayList;
 
 
 public class ScatterPlot extends JPanel {
 
     private final Data data;
-    private int index;
     private Color color = Color.ORANGE;
     private double xAxis;
     private double yAxis;
@@ -18,45 +19,67 @@ public class ScatterPlot extends JPanel {
     private double zeroYAxis;
     private double width;
     private double height;
+    private ArrayList<Double> xCoordinates;
+    private ArrayList<Double> yCoordinates;
+    private double xValue;
+    private double yValue;
+    private double xCoordinate;
+    private double yCoordinate;
+    private final int OFFSET;
+
+
 
 
     public ScatterPlot(Data data) {
 
         this.data = data;
+        this.OFFSET = 80;
         setBackground(Color.WHITE);
-        setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK), data.getDataContent().get(index).getVariableName()));
-        this.xAxis = (getWidth() - getInsets().right);
-        this.yAxis = getHeight() / data.getDataContent().get(index).getVariableContent().size() * data.getDataContent().get(index).getMaxValue();
-        this.zeroXAxis = getInsets().left;
-        this.zeroYAxis = getInsets().top;
+        setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK), data.getDataContent().get(0).getVariableName()));
         this.width = getWidth();
         this.height = getHeight();
+        this.xAxis = data.getDataContent().get(0).getMaxValue();
+        this.yAxis = data.getDataContent().get(1).getMaxValue();
+        this.zeroXAxis = getInsets().left;
+        this.zeroYAxis = getInsets().top;
+        this.xCoordinates = new ArrayList<>();
+        this.yCoordinates = new ArrayList<>();
 
+
+        for (int i = 0; i < data.getDataContent().get(0).getVariableContent().size(); i++) {
+
+            this.xValue = data.getDataContent().get(0).getVariableContent().get(i);
+            xCoordinates.add(i, xValue);
+
+        }
+        for (int i = 0; i < data.getDataContent().get(1).getVariableContent().size(); i++) {
+            this.yValue = data.getDataContent().get(1).getVariableContent().get(i);
+            yCoordinates.add(i, yValue);
+
+        }
 
     }
-
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        double xSize = 10;
+        double ySize = 10;
 
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(Color.BLACK);
+        //AffineTransform transform = new AffineTransform();
+        //transform.setToRotation(Math.PI / 2);
+        //g2.setTransform(transform);
         Line2D xLine = new Line2D.Double(zeroXAxis, zeroYAxis, xAxis, zeroYAxis);
         g2.draw(xLine);
         Line2D yLine = new Line2D.Double(zeroXAxis, zeroYAxis, zeroXAxis, yAxis);
         g2.draw(yLine);
+        g2.setColor(Color.BLACK);
         //g2.setStroke(new java.awt.BasicStroke(6, BasicStroke.JOIN_ROUND, BasicStroke.CAP_ROUND, 1.0f, new float[]{1}, 0.0f));
 
 
-        double xSize = width / data.getDataContent().get(index).getVariableContent().size();
-        double ySize = height / data.getDataContent().get(index).getVariableContent().size();
-        g2.setPaint(Color.BLACK);
-        for (int i = 0; i < data.getDataContent().size(); i++) {
-            double value = data.getDataContent().get(index).getVariableContent().get(i);
-            double x = (value + zeroXAxis);
-            double y = (value + zeroYAxis);
-            Ellipse2D ellipse = new Ellipse2D.Double(x, y, xSize, ySize);
+        for (int i = 0; i < xCoordinates.size(); i++) {
+            Ellipse2D ellipse = new Ellipse2D.Double(getxCoordinates(i), getyCoordinates(i), xSize, ySize);
             g2.fill(ellipse);
             g2.draw(ellipse);
             //int x = (int) (value / data.getDataContent().get(index).getMaxValue());
@@ -67,11 +90,21 @@ public class ScatterPlot extends JPanel {
 
 
 
+    }
 
+    public double getxCoordinates(int i) {
+        {
+            this.xCoordinate = xCoordinates.get(i);
 
-
-
-
+        }
+        return xCoordinate;
 
     }
+
+    public double getyCoordinates(int i) {
+        this.yCoordinate = yCoordinates.get(i);
+        return yCoordinate;
+    }
+
+
 }
