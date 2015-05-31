fc7dad974d6370f291146246b86d6e67b6ce3fbc
Wuestenfuchs88 <sven.stamm@students.fhnw.ch>
2015-05-31 23:08:17 +0200

    - Update Scatterplot: Linienverbindung funktioniert nun.

--- src/main/java/analyzer/gui/ScatterPlot.java
+++ src/main/java/analyzer/gui/ScatterPlot.java
@@ -3,8 +3,6 @@ package analyzer.gui;
 import analyzer.datastore.Data;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Line2D;
 import java.util.ArrayList;
 
 
@@ -12,12 +10,6 @@ public class ScatterPlot extends JPanel {
 
     private final Data data;
     private Color color = Color.ORANGE;
-    private double xAxis;
-    private double yAxis;
-    private double zeroXAxis;
-    private double zeroYAxis;
-    private double width;
-    private double height;
     private ArrayList<Double> xCoordinates;
     private ArrayList<Double> yCoordinates;
     private double xValue;
@@ -26,20 +18,12 @@ public class ScatterPlot extends JPanel {
     private double yCoordinate;
 
 
-
-
-
     public ScatterPlot(Data data) {
 
         this.data = data;
         setBackground(Color.WHITE);
         setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK), data.getDataContent().get(0).getVariableName()));
-        /*this.width = getWidth();
-        this.height = getHeight();
-        this.xAxis = data.getDataContent().get(0).getMaxValue();
-        this.yAxis = data.getDataContent().get(1).getMaxValue();
-        this.zeroXAxis = getHeight() - getInsets().bottom;
-        this.zeroYAxis = getHeight() - getInsets().bottom;
+        /*
         this.xCoordinates = new ArrayList<>();
         this.yCoordinates = new ArrayList<>();*/
 
@@ -68,30 +52,30 @@ public class ScatterPlot extends JPanel {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setColor(Color.BLUE);
+        FontMetrics fontMetrics = getFontMetrics(getFont());
 
         for (int i = 0; i < data.getDataContent().get(0).getVariableContent().size(); i++) {
             g2.fillOval(((int) ((((data.getDataContent().get(0).getVariableContent().get(i) -
-                    data.getDataContent().get(0).getMinValue()) / data.getDataContent().get(0).getRange()) * OFFSETY) + (OFFSETX / 2)))
-                    , ((int) (OFFSETY - (((data.getDataContent().get(1).getVariableContent().get(i) - data.getDataContent().get(1).getMinValue())
+                    data.getDataContent().get(0).getMinValue()) / data.getDataContent().get(0).getRange())
+                    * OFFSETY) + (OFFSETX / 2)))
+                    , ((int) (OFFSETY - (((data.getDataContent().get(1).getVariableContent().get(i)
+                    - data.getDataContent().get(1).getMinValue())
                     / data.getDataContent().get(1).getRange()) * OFFSETY) + (OFFSETY / 4)))
                     , xSize, ySize);
-
-            System.out.println(getWidth());
 
         }
 
         for (int j = 0; j < data.getDataContent().get(0).getVariableContent().size() - 1; j++) {
             g2.drawLine(((int) ((((data.getDataContent().get(0).getVariableContent().get(j) -
                             data.getDataContent().get(0).getMinValue()) / data.getDataContent().get(0).getRange()) * OFFSETY) + (OFFSETX / 2))),
-                    ((int) (OFFSETY - ((((data.getDataContent().get(1).getVariableContent().get(j) - data.getDataContent().get(1).getMinValue())
+                    ((int) ((OFFSETY - (((data.getDataContent().get(1).getVariableContent().get(j) - data.getDataContent().get(1).getMinValue())
                             / data.getDataContent().get(1).getRange()) * OFFSETY) + (OFFSETY / 4)))),
                     ((int) ((((data.getDataContent().get(0).getVariableContent().get(j + 1)
                             - data.getDataContent().get(0).getMinValue()) / data.getDataContent().get(0).getRange()) * OFFSETY) + (OFFSETX / 2))),
-                    ((int) ((((data.getDataContent().get(1).getVariableContent().get(j + 1) - data.getDataContent().get(1).getMinValue())
+                    ((int) ((OFFSETY - ((data.getDataContent().get(1).getVariableContent().get(j + 1) - data.getDataContent().get(1).getMinValue())
                             / data.getDataContent().get(1).getRange()) * OFFSETY) + (OFFSETY / 4))));
-
         }
-
+    }
 
     /*public double getxCoordinates(int i) {
         {
@@ -109,5 +93,5 @@ public class ScatterPlot extends JPanel {
     }*/
         //public void setColor ()
 
-    }
+
 }
