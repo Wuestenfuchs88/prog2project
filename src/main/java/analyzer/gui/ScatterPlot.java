23d07c6a2b92ff05fc70fc8458fb5ec58643fd80
Wuestenfuchs88 <sven.stamm@students.fhnw.ch>
2015-06-03 20:25:26 +0200

    - Update Scatterplot

--- src/main/java/analyzer/gui/ScatterPlot.java
+++ src/main/java/analyzer/gui/ScatterPlot.java
@@ -22,76 +22,39 @@ public class ScatterPlot extends JPanel {
 
         this.data = data;
         setBackground(Color.WHITE);
-        setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK), data.getDataContent().get(0).getVariableName()));
-        /*
-        this.xCoordinates = new ArrayList<>();
-        this.yCoordinates = new ArrayList<>();*/
-
-
-        /*for (int i = 0; i < data.getDataContent().get(0).getVariableContent().size(); i++) {
-
-            this.xValue = data.getDataContent().get(0).getVariableContent().get(i);
-            xCoordinates.add(i, xValue);
-
-        }
-        for (int i = 0; i < data.getDataContent().get(1).getVariableContent().size(); i++) {
-            this.yValue = data.getDataContent().get(1).getVariableContent().get(i);
-            yCoordinates.add(i, yValue);
-
-        }*/
 
     }
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        int xSize = 10;
-        int ySize = 10;
-        final int OFFSETY = 260;
-        final int OFFSETX = 590;
+        int xSize = 3;
+        int ySize = 3;
 
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setColor(Color.BLUE);
-        FontMetrics fontMetrics = getFontMetrics(getFont());
 
         for (int i = 0; i < data.getDataContent().get(0).getVariableContent().size(); i++) {
             g2.fillOval(((int) ((((data.getDataContent().get(0).getVariableContent().get(i) -
                     data.getDataContent().get(0).getMinValue()) / data.getDataContent().get(0).getRange())
-                    * OFFSETY) + (OFFSETX / 2)))
-                    , ((int) (OFFSETY - (((data.getDataContent().get(1).getVariableContent().get(i)
+                    * (getWidth() - 2))))
+                    , ((int) (getHeight() - ((((data.getDataContent().get(1).getVariableContent().get(i)
                     - data.getDataContent().get(1).getMinValue())
-                    / data.getDataContent().get(1).getRange()) * OFFSETY) + (OFFSETY / 4)))
+                    / data.getDataContent().get(1).getRange()) * (getHeight() - 2)))))
                     , xSize, ySize);
 
         }
 
         for (int j = 0; j < data.getDataContent().get(0).getVariableContent().size() - 1; j++) {
             g2.drawLine(((int) ((((data.getDataContent().get(0).getVariableContent().get(j) -
-                            data.getDataContent().get(0).getMinValue()) / data.getDataContent().get(0).getRange()) * OFFSETY) + (OFFSETX / 2))),
-                    ((int) ((OFFSETY - (((data.getDataContent().get(1).getVariableContent().get(j) - data.getDataContent().get(1).getMinValue())
-                            / data.getDataContent().get(1).getRange()) * OFFSETY) + (OFFSETY / 4)))),
+                            data.getDataContent().get(0).getMinValue()) / data.getDataContent().get(0).getRange()) * (getWidth() - 2)))),
+                    ((int) ((getHeight() - (((data.getDataContent().get(1).getVariableContent().get(j) - data.getDataContent().get(1).getMinValue())
+                            / data.getDataContent().get(1).getRange()) * (getHeight() - 2))))),
                     ((int) ((((data.getDataContent().get(0).getVariableContent().get(j + 1)
-                            - data.getDataContent().get(0).getMinValue()) / data.getDataContent().get(0).getRange()) * OFFSETY) + (OFFSETX / 2))),
-                    ((int) ((OFFSETY - ((data.getDataContent().get(1).getVariableContent().get(j + 1) - data.getDataContent().get(1).getMinValue())
-                            / data.getDataContent().get(1).getRange()) * OFFSETY) + (OFFSETY / 4))));
+                            - data.getDataContent().get(0).getMinValue()) / data.getDataContent().get(0).getRange()) * (getWidth() - 2)))),
+                    ((int) ((getHeight() - ((data.getDataContent().get(1).getVariableContent().get(j + 1) - data.getDataContent().get(1).getMinValue())
+                            / data.getDataContent().get(1).getRange()) * (getHeight() - 2)))));
         }
     }
-
-    /*public double getxCoordinates(int i) {
-        {
-            this.xCoordinate = xCoordinates.get(i);
-
-        }
-        return xCoordinate;
-
-    }
-
-    public double getyCoordinates(int i) {
-        this.yCoordinate = yCoordinates.get(i);
-        yCoordinate = getHeight() - yCoordinate;
-        return yCoordinate;
-    }*/
-        //public void setColor ()
-
 
 }
