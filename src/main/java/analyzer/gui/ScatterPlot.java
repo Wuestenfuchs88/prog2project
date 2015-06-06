7ff3e5042a04e927b38cd86b900ef18b936c5f26
Josh <joshua.pante@students.fhnw.ch>
2015-06-06 21:11:26 +0200

    pfahlbau-koordinatenpfeile mit labels hinzugefügt..

--- src/main/java/analyzer/gui/ScatterPlot.java
+++ src/main/java/analyzer/gui/ScatterPlot.java
@@ -19,7 +19,7 @@ public class ScatterPlot extends JPanel {
         this.yIndex = yIndex;
         this.zIndex = zIndex;
         setBackground(Color.WHITE);
-        //setBorder(BorderFactory.createTitledBorder(data.()));
+
     }
     @Override
     protected void paintComponent(Graphics g) {
@@ -31,6 +31,22 @@ public class ScatterPlot extends JPanel {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setColor(color);
+
+        // x-axis
+        g2.drawLine(10, getHeight() - 10, getWidth() - 75, getHeight() - 10);
+        // y-axis
+        g2.drawLine(10, getHeight() - 10, 10, 20);
+        // x-axis arrow
+        g2.drawLine(getWidth() - 75 - 5, getHeight() - 10 - 5, getWidth() - 75, getHeight() - 10);
+        g2.drawLine(getWidth() - 75 - 5, getHeight() - 10 + 5, getWidth() - 75, getHeight() - 10);
+        // y-axis arrow
+        g2.drawLine(10 - 5, 20 + 5, 10, 20);
+        g2.drawLine(10 + 5, 20 + 5, 10, 20);
+        // draw origin Point
+        g2.fillOval(7, getHeight() - 13, 6, 6);
+        // draw labels
+        g2.drawString(data.getDataContent().get(xIndex).getVariableName(), getWidth() - 70, getHeight() - 5);
+        g2.drawString(data.getDataContent().get(yIndex).getVariableName(), 5, 10);
 
         for (int i = 0; i < data.getDataContent().get(xIndex).getVariableContent().size(); i++) {
             if (pointWeight) {
